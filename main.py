abf316e21dbff84ef4edf5c8dcd921948ee62548
aryadaroui <darouiarya@gmail.com>
2019-11-04 21:43:40 -0800

    added inverse and reverse. added basic commandline dialogue with tiny amount of user input checking

--- main.py
+++ main.py
@@ -2,37 +2,74 @@ import audioop
 import aifc
 from itertools import repeat
 from math import ceil
+from numpy import linspace
 from functions import * # pylint: disable=unused-wildcard-import
 
-Clear()
-
-NORM = 0
+INTRO = "logica v0.5 -- written by Arya Daroui\n\nlogica applies offline audio effects to an input .aif file:\ninverse: reverse chunks but preserve order\nconverse: chunks not reversed, but their order is\nreverse (contrapositive): reversed chunks and reversed order, effectively reversing the entire audio file\n"
 INV = 1
 CONV = 2
 REV = 3
 
-inFile = aifc.open("test.aif", 'r')
-outFile = aifc.open("output.aif", 'w')
+Clear()
+print(INTRO)
+inFilePath = input("please drag and drop the input .aif file: ")
+inFilePath = inFilePath.rstrip()	# removes whitespace
+inFilePath = inFilePath.rstrip('/')	# removes extra / at end
+inFilePath = inFilePath.replace(r'\ ', ' ')	# removes extra escape character prefix
+inFile = aifc.open(inFilePath, 'r')
+
+
+Clear()
+badMode = True
+while badMode:
+	mode = input("1 inverse\n2 converse\n3 reverse\nSelect mode: ")
+	if mode == '1':
+		modeStr = "-inverse"
+		badMode = False
+	elif mode == '2':
+		modeStr = "-converse"
+		badMode = False
+	elif mode == '3':
+		modeStr = "-reverse"
+		badMode = False
+	else:
+		badMode = True
+
+outfilePath = inFilePath.replace(".aif", modeStr + ".aif")
+outFile = aifc.open(outfilePath, 'w')
 outFile.aiff()
 outFile.setparams(inFile.getparams())
 
-PrintInfo(inFile)
 
-# bpm = input("BPM: ")
-# chunkRate = input("chunk factor: ")
-# logicMode = input("logic mode: ")
-bpm = 120
-chunkiness = .3
-logicMode = CONV
+# PrintInfo(inFile)
 
-chunkSize = int(bpm / 60 * outFile.getframerate() * chunkiness)
+Clear()
+bpm = int(input("Tempo BPM: "))
+chunkiness = int(input("chunkiness (chunk per beat): "))
+# bpm = 120
+# chunkiness = .125
+# logicMode = CONV
 
-for _ in repeat(None, ceil(inFile.getnframes() / chunkSize)):
-	chunk = inFile.readframes(chunkSize)
-	outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
+chunkSize = int((bpm / 60 * outFile.getframerate()) / chunkiness)
 
-
+if mode == '1':
+	# CONVERSE
+	for _ in repeat(None, ceil(inFile.getnframes() / chunkSize)):
+		chunk = inFile.readframes(chunkSize)
+		outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
+elif mode == '2':
+	#INVERSE
+	for position in reversed(range(0, inFile.getnframes(), chunkSize)):
+		inFile.setpos(position)
+		chunk = inFile.readframes(chunkSize)
+		outFile.writeframes(chunk)
+else:
+	#REVERSE
+	for position in reversed(range(0, inFile.getnframes(), chunkSize)):
+		inFile.setpos(position)
+		chunk = inFile.readframes(chunkSize)
+		outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
 
 inFile.close()
 outFile.close()
-print("END\n")
+print("OUTPUT at " + outfilePath)
