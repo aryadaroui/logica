c7582d11c7d52603dfb8ac316f931102fa9782e1
aryadaroui <darouiarya@gmail.com>
2019-11-05 01:12:22 -0800

    fixed time signature math. added diverse and triverse effects

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ INTRO = "logica v0.5 -- written by Arya Daroui\n\nlogica applies offline audio e
 INV = 1
 CONV = 2
 REV = 3
+TRIV = 4
 
 Clear()
 print(INTRO)
@@ -22,7 +23,7 @@ inFile = aifc.open(inFilePath, 'r')
 Clear()
 badMode = True
 while badMode:
-	mode = input("1 inverse\n2 converse\n3 reverse\nSelect mode: ")
+	mode = input("1 inverse\n2 converse\n3 reverse\n4 triverse\n5 diverse\nSelect mode: ")
 	if mode == '1':
 		modeStr = "-inverse"
 		badMode = False
@@ -32,25 +33,37 @@ while badMode:
 	elif mode == '3':
 		modeStr = "-reverse"
 		badMode = False
+	elif mode == '4':
+		modeStr = "-triverse"
+		badMode = False
+	elif mode == '5':
+		modeStr = "-diverse"
+		badMode = False
 	else:
+		Clear()
 		badMode = True
-
-outfilePath = inFilePath.replace(".aif", modeStr + ".aif")
-outFile = aifc.open(outfilePath, 'w')
-outFile.aiff()
-outFile.setparams(inFile.getparams())
-
 
 # PrintInfo(inFile)
 
 Clear()
 bpm = int(input("Tempo BPM: "))
-chunkiness = int(input("chunkiness (chunk per beat): "))
+chunkiness = float(input("chunkiness (chunk per beat): "))
+# input if diverse and triverse should start on reverse
+
+# if mode = '5':
+# 	startRev = input
 # bpm = 120
 # chunkiness = .125
 # logicMode = CONV
 
-chunkSize = int((bpm / 60 * outFile.getframerate()) / chunkiness)
+outfilePath = inFilePath.replace(".aif", modeStr + "-" + str(bpm) + "-" + str(chunkiness) + ".aif")
+outFile = aifc.open(outfilePath, 'w')
+outFile.aiff()
+outFile.setparams(inFile.getparams())
+
+# chunkSize = int((bpm / 60 * outFile.getframerate()) )
+
+chunkSize = int(outFile.getframerate() * 60 * (1/bpm) * (1/chunkiness))
 
 if mode == '1':
 	# CONVERSE
@@ -63,12 +76,34 @@ elif mode == '2':
 		inFile.setpos(position)
 		chunk = inFile.readframes(chunkSize)
 		outFile.writeframes(chunk)
-else:
+	# could have also just reversed (contrapositiv-ed) the converse
+elif mode == '3':
 	#REVERSE
 	for position in reversed(range(0, inFile.getnframes(), chunkSize)):
 		inFile.setpos(position)
 		chunk = inFile.readframes(chunkSize)
 		outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
+elif mode == '4':
+	#TRIVERSE
+	for _ in repeat(None, ceil(inFile.getnframes() / chunkSize)):
+		chunk = inFile.readframes(chunkSize)
+		outFile.writeframes(chunk)
+		outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
+		outFile.writeframes(chunk)
+elif mode == '5':
+	#DIVERSE
+	for count in range(0, ceil(inFile.getnframes() / chunkSize)):
+		chunk = inFile.readframes(chunkSize)
+		if count % 2 == 1:
+			# print(str(count) + "RN")
+			outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
+			outFile.writeframes(chunk)
+		else:
+			# print(str(count) + "NR")
+			outFile.writeframes(chunk)
+			outFile.writeframes(audioop.reverse(chunk, outFile.getsampwidth()))
+
+
 
 inFile.close()
 outFile.close()
